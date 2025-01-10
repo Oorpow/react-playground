fa0214ae02fdd2a15a4dbc136521582f9343dff6
Oorpow <309754513@qq.com>
2025-01-10 10:21:31 +0800

    feat: ✨ 添加文件至文件区

--- src/components/EditorFileList.tsx
+++ src/components/EditorFileList.tsx
@@ -1,4 +1,5 @@
 import { useContext, useEffect, useRef, useState } from 'react';
+import { Plus } from 'lucide-react';
 import { PlaygroundContext } from '../store/PlaygroundContext';
 
 export interface FileItemProps {
@@ -6,20 +7,28 @@ export interface FileItemProps {
 	value: string;
 	/** 选中状态  */
 	isActive: boolean;
+	/** 是否点击创建按钮，添加文件至文件区 */
+	isCreating: boolean;
 	onClick: () => void;
 	/**
 	 * 编辑事件
 	 * @param name 新的文件名
-	 * @returns 
+	 * @returns
 	 */
-	onEditFinished: (name: string) => void
+	onEditFinished: (name: string) => void;
 }
 
 function FileItem(props: FileItemProps) {
-	const { value, onClick, isActive = false, onEditFinished } = props;
+	const {
+		value,
+		onClick,
+		isActive = false,
+		isCreating: propsIsCreating,
+		onEditFinished,
+	} = props;
 	// 文件名
 	const [name, setName] = useState(value);
-	const [isEditing, setIsEditing] = useState(false);
+	const [isEditing, setIsEditing] = useState(propsIsCreating);
 	const inputRef = useRef<HTMLInputElement>(null);
 
 	/** 双击文件区item，变成输入框，允许修改文件名 */
@@ -35,6 +44,13 @@ function FileItem(props: FileItemProps) {
 		onEditFinished(name);
 		setIsEditing(false);
 	};
+
+	// 默认将最后一个tab作为编辑状态
+	useEffect(() => {
+		if (propsIsCreating) {
+			inputRef.current?.focus();
+		}
+	}, [propsIsCreating]);
 
 	return (
 		<div
@@ -68,26 +84,39 @@ function EditorFileList() {
 	} = useContext(PlaygroundContext);
 
 	const [tabs, setTabs] = useState(['']);
+	const [isCreating, setIsCreating] = useState(false);
 
 	useEffect(() => {
 		// 文件名作为tabs数组项
 		setTabs(Object.keys(files));
 	}, [files]);
 
-
 	/**
 	 * 文件区 文件名编辑事件
-	 * @param oldFileName 
-	 * @param newFileName 
+	 * @param oldFileName
+	 * @param newFileName
 	 */
 	function handleEditFinished(oldFileName: string, newFileName: string) {
-		updateFileName(oldFileName, newFileName)
-		setSelectedFileName(newFileName)
+		updateFileName(oldFileName, newFileName);
+		setSelectedFileName(newFileName);
+		setIsCreating(false);
+	}
+
+	/**
+	 * 添加文件到文件区
+	 * 点击 + 时，进入isCreating状态，添加一个file到文件区，并将最后一个tab设为编辑状态
+	 */
+	function addFileToTabs() {
+		const newFileName =
+			'Comp' + Math.random().toString().slice(2, 8) + '.tsx';
+		addFile(newFileName);
+		setSelectedFileName(newFileName);
+		setIsCreating(true);
 	}
 
 	return (
-		<div className="flex m-1 overflow-x-auto overflow-y-hidden flex-nowrap">
-			{tabs.map((item, idx) => {
+		<div className="flex items-center m-1 overflow-x-auto overflow-y-hidden flex-nowrap">
+			{tabs.map((item, idx, arr) => {
 				// 如果不过滤空值，则会导致首次渲染时，第一项不渲染
 				if (!item) return null;
 				// 选中时修改文件名，随着文件名发生变化，其他组件追踪到文件名变化会自动查找文件名对应的文件
@@ -96,11 +125,21 @@ function EditorFileList() {
 						value={item}
 						key={idx}
 						isActive={selectedFileName === item}
+						// 默认将最后一个tab作为编辑状态
+						isCreating={isCreating && idx === arr.length - 1}
 						onClick={() => setSelectedFileName(item)}
-						onEditFinished={(name: string) => handleEditFinished(item, name)}
+						onEditFinished={(name: string) =>
+							handleEditFinished(item, name)
+						}
 					/>
 				);
 			})}
+			<div
+				className="cursor-pointer hover:bg-slate-400"
+				onClick={addFileToTabs}
+			>
+				<Plus />
+			</div>
 		</div>
 	);
 }
